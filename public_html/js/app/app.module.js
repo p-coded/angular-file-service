418bc246ff15f16fa30c839a55f0602dfae4d6db
Patrick Wettlaufer <patrick.wettlaufer@gmx.de>
2017-08-28 15:59:49 +0200

    removed dependencies that belong to FileService

--- public_html/js/app/app.module.js
+++ public_html/js/app/app.module.js
@@ -1,14 +1,12 @@
-"use strict";
-
 (function ()
 {
+    "use strict";
     // Lade alle abhängigen Module (dependencies)
-    angular.module('pFile', ['ngRoute', 'ngAnimate', 'ui.bootstrap', 'pFile.filter', 'pFile.services', 'pFile.views']);
+    angular.module('app', ['ngRoute', 'ngAnimate', 'ui.bootstrap', 'app.views', 'pFile']);
 
-    
     // AngularJS bootstrap an Dokument binden; Alternativ zu ng-app='app' in der index.html
     angular.element(document).ready(function () {
         console.log("document ready. starting angular.bootstrap", new Date());
-        angular.bootstrap(document, ['pFile']);
+        angular.bootstrap(document, ['app']);
     });
 })();
